fb8db8cc357362535c897e9788e4fcce7fc1111f
Sayan Chakraborty <aiudd75@USWSA0138139.local>
2019-08-04 14:09:32 +0530

    2nd

--- script.js
+++ script.js
@@ -1,25 +1,21 @@
 async function init() {
 
-    const yearAndPrice = await d3.csv("year-price-all.csv");
-    const yearmonthvolume = await d3.csv("year-month-volume.csv");
-    const monthyearregionconv = await d3.csv("month-year-region-conv.csv");
+
+
+const yearAndPrice = await d3.csv("year-price-all.csv");
+const yearmonthvolume = await d3.csv("year-month-volume.csv");
+const monthyearregionconv = await d3.csv("month-year-region-conv.csv");
 
  
-    var margin = {top: 30, right: 30, bottom: 70, left: 60},
-    width = 460 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
+var margin = {top: 30, right: 30, bottom: 70, left: 60},
+width = 460 - margin.left - margin.right,
+height = 400 - margin.top - margin.bottom;
     
    var conventional = yearAndPrice.filter(function(d){
                                             return (d.Type ==='conventional')
                                         }) 
                                 
-    var organic = yearAndPrice.filter(function(d){
-                                            if (d.type == 'organic')
-                                            return {
-                                                year: d.year,
-                                                avgPrice: d.avgPrice
-                                              }
-                                        })
+
 
 //console.log(conventional);
 
@@ -48,15 +44,23 @@ svg.append("g")
         .attr("class", "myYaxis")
         .call(d3.axisLeft(y));
 
+var tooltip = d3.select("#tooltip");
+
 svg.selectAll("rect")
     .data(conventional)
     .enter()
     .append("rect") // Add a new rect for each new elements
     .attr("x", function(d) { return x(d.Year); })
-     .attr("y", function(d) { return y(d.AvgPrice); })
-     .attr("width", x.bandwidth())
-     .attr("height", function(d) { return height - y(d.AvgPrice); })
-     .attr("fill", "#69b3a2")
+    .attr("y", function(d) { return y(d.AvgPrice); })
+    .attr("width", x.bandwidth())
+    .attr("height", function(d) { return height - y(d.AvgPrice); })
+    .attr("fill", "#69b3a2")
+    .on('mouseover', function(d,i) {
+        tooltip.style("opacity", 1)
+                .style("left",(d3.event.pageX)+"px")
+                .style("top",(d3.event.pageY)+"px")
+                .html("Price $"+truncateDecimals(d.AvgPrice,2)+" in Year "+d.Year);})
+    .on("mouseout", function() { tooltip.style("opacity", 0) })
     .on("click", function(d,i){
         console.log("click");
         document.getElementById("bymonth").innerHTML = "";
@@ -107,6 +111,8 @@ svgMonth.append("g")
 
 console.log(monthVolume);        
 
+var tooltip = d3.select("#tooltip");
+
 svgMonth.selectAll("rect")
         .data(monthVolume)
         .enter()
@@ -116,6 +122,13 @@ svgMonth.selectAll("rect")
          .attr("width", x.bandwidth())
          .attr("height", function(d) { return height - y(d.logVolume); })
          .attr("fill", "#69b3a2")
+         .on('mouseover', function(d,i) {
+            console.log("mouse");
+            tooltip.style("opacity", 1)
+                    .style("left",(d3.event.pageX)+"px")
+                    .style("top",(d3.event.pageY)+"px")
+                    .html("Volume "+d.Volume+" Units in Month "+d.Month);})
+        .on("mouseout", function() { tooltip.style("opacity", 0) })
          .on("click", function(d,i){
             console.log("click");
             document.getElementById("byregion").innerHTML = "";
@@ -172,4 +185,86 @@ svgRegion.selectAll("rect")
             .attr("width", function(d) { return x(d.Volume); })
             .attr("fill", "#69b3a2");
  
+}
+
+function updateC(){
+    console.log("Now OK?");
+
+}
+
+async function updateO(){
+
+    document.getElementById("bymonth").innerHTML = "";
+    document.getElementById("byregion").innerHTML = "";
+    document.getElementById("my_dataviz").innerHTML = "";
+    const yearAndPrice = await d3.csv("year-price-all.csv");
+
+    var margin = {top: 30, right: 30, bottom: 70, left: 60},
+        width = 460 - margin.left - margin.right,
+        height = 400 - margin.top - margin.bottom;
+    
+    var organic = yearAndPrice.filter(function(d){
+                        return (d.Type = 'organic');
+        })
+    console.log(organic);
+
+    var svg = d3.select("#my_dataviz")
+    .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+        .attr("transform","translate(" + margin.left + "," + margin.top + ")");
+
+
+ // X axis
+var x = d3.scaleBand()
+            .range([ 0, width ])
+            .domain(yearAndPrice.map(function(d) { return d.Year; }))
+            .padding(0.2);
+svg.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x))
+
+// Add Y axis
+var y = d3.scaleLinear()
+            .domain([1, 2])
+            .range([ height, 0]);
+svg.append("g")
+        .attr("class", "myYaxis")
+        .call(d3.axisLeft(y));
+
+var tooltip = d3.select("#tooltip");
+
+svg.selectAll("rect")
+    .data(organic)
+    .enter()
+    .append("rect") // Add a new rect for each new elements
+    .attr("x", function(d) { return x(d.Year); })
+    .attr("y", function(d) { return y(d.AvgPrice); })
+    .attr("width", x.bandwidth())
+    .attr("height", function(d) { return height - y(d.AvgPrice); })
+    .attr("fill", "#69b3a2")
+    .on('mouseover', function(d,i) {
+        tooltip.style("opacity", 1)
+                .style("left",(d3.event.pageX)+"px")
+                .style("top",(d3.event.pageY)+"px")
+                .html("Price $"+truncateDecimals(d.AvgPrice,2)+" in Year "+d.Year);})
+    .on("mouseout", function() { tooltip.style("opacity", 0) })
+    .on("click", function(d,i){
+        console.log("click");
+        document.getElementById("bymonth").innerHTML = "";
+        byMonth(d, yearmonthvolume, monthyearregionconv); 
+        document.getElementById("byregion").innerHTML = "";
+        });
+}
+
+
+function truncateDecimals (num, digits) {
+    var numS = num.toString(),
+        decPos = numS.indexOf('.'),
+        substrLength = decPos == -1 ? numS.length : 1 + decPos + digits,
+        trimmedResult = numS.substr(0, substrLength),
+        finalResult = isNaN(trimmedResult) ? 0 : trimmedResult;
+
+    return parseFloat(finalResult);
 }
